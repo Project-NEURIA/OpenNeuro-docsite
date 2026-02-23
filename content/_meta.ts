5552507bec9f2ebe7ebc8a94e6bdf00bc53f924a
xingjianll <4396kevinliu@gmail.com>
2026-02-23 15:58:36 -0500

    update

---
+++ content/_meta.ts
@@ -0,0 +1,17 @@
+export default {
+  index: {
+    title: "OpenNeuro",
+    type: "page",
+    theme: {
+      layout: "raw",
+    },
+  },
+  "user-manual": {
+    title: "User Manual",
+    type: "page",
+  },
+  "developer-guide": {
+    title: "Developer Guide",
+    type: "page",
+  },
+};
